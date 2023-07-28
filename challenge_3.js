835e7bbe5c5d4ccd6de36e43e71676a83454aa1b
AlishaTariq1122 <140748795+AlishaTariq1122@users.noreply.github.com>
2023-07-28 20:40:08 +0500

    Add files via upload

---
+++ challenge_3.js
@@ -0,0 +1,11 @@
+const variable1 = 20;
+const variable2 = '40';
+
+const resultVariable = variable1 + variable2;
+
+console.log(resultVariable);
+
+// The resultVariable will print "2040", and it is a string.
+
+// This will return "string", which tells us that the variable is a string data type
+typeof resultVariable
---
+++ challenge_7.js
@@ -0,0 +1,30 @@
+// Block 1
+
+/* const firstNumber = 20;
+const secondNumber = '20';
+
+const result = firstNumber === secondNumber;
+
+console.log(result); */
+
+// Block 2
+
+/* const firstNumber = 20;
+const secondNumber = '20';
+
+const result = firstNumber == secondNumber;
+
+console.log(result); */
+
+/* The following two blocks of code do not result in the same answer. The first code block will evaluate to false while the second code
+ block will evaluate to true.*/
+
+ /*Which one is better? When looking at this code, there is no reason why secondNumber should be a
+  string data type.*/
+
+const firstNumber = 20;
+const secondNumber = 20;
+
+const result = firstNumber === secondNumber;
+
+console.log(result);
