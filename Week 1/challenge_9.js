9c697dc96d4baf727b1723162d3724539e01a799
AlishaTariq1122 <140748795+AlishaTariq1122@users.noreply.github.com>
2023-08-05 21:17:03 +0500

    Add files via upload

---
+++ Week 1/challenge_9.js
@@ -0,0 +1,19 @@
+/* const myObj = {
+  prop1: 'first value',
+  prop2: 20
+};
+
+// Index         0   1   2
+const myArray = [40, 50, 2];
+
+const result = myObj.prop2 === (myArray[0] / myArray[2]);*/
+
+// What does result evaluate to?
+
+/* myObj.prop2 equals 20
+   myArray[0] equals 40
+   myArray[2] equals 2 */
+
+   const result = 20 === (40 / 2); // true
+
+  // 40 / 2 evaluates to 20, and 20 === 20 evaluates to true.
