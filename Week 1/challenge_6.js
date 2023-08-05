e5ac931175821fa0c76c25f59d682524d8fd053f
AlishaTariq1122 <140748795+AlishaTariq1122@users.noreply.github.com>
2023-08-05 21:15:27 +0500

    Add files via upload

---
+++ Week 1/challenge_6.js
@@ -0,0 +1,20 @@
+/* const numberVariable = 0;
+
+numberVariable++;
+numberVariable++;
+numberVariable++;
+
+console.log(numberVariable); */
+
+
+/* This code will not work because we are using const. Remember, if the value of the variable is re-assigned in your code, you need to use the 
+let keyword to declare the variable. Once we change it to let, the code works and numberVariable will be equal to 3. 
+Each time we use the expression numberVariable++, our variable is incremented by 1, and re-assigned the new value. */
+
+let numberVariable = 0;
+
+numberVariable++;
+numberVariable++;
+numberVariable++;
+
+console.log(numberVariable); // 3
